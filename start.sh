e70830d1b19359f6c309ac17d905d0d507c65dc9
Ammus0033 <106924606+Ammus0033@users.noreply.github.com>
2022-12-04 01:52:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Pooja0033/Prv_35.git /Prv_35
+  git clone https://github.com/Ammus0033/Honey-Rose.git /Prv_35
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Prv_35
+  git clone $UPSTREAM_REPO /Honey-Rose
 fi
-cd /Prv_35
+cd /Honey-Rose
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
