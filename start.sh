14049f67d6f5848a60af42196c381b2559f050ef
Ammus0033 <106924606+Ammus0033@users.noreply.github.com>
2022-12-04 01:53:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Ammus0033/Honey-Rose.git /Prv_35
+  git clone https://github.com/Ammus0033/Honey-Rose.git /Honey-Rose
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Honey-Rose
